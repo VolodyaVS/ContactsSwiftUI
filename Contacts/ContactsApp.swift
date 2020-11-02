a6a57d84f5a451c06e8dc64930896ac97f3a88bc
Volodya <volodyavolga@protonmail.ch>
2020-11-02 08:30:14 +0300

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Vladimir Stepanchikov on 02.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
